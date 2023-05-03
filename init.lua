4337dcd5bd7260adf4eedad10f2d656cc02d7084
Tomas Jira <tom.jira@gmail.com>
2023-05-03 13:30:11 +0200

    change colorscheme

--- init.lua
+++ init.lua
@@ -1,5 +1,5 @@
 return {
-    colorscheme = "catppuccin",
+    colorscheme = "nightfox",
 
     lsp = {
         formatting = {
--- plugins/user.lua
+++ plugins/user.lua
@@ -1,3 +1,3 @@
 return {
-    "catppuccin/nvim"
+    "EdenEast/nightfox.nvim"
 }
