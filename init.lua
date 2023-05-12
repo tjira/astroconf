9253b4b9660065e2295d940e86df2d99d8723689
Tomas Jira <tom.jira@gmail.com>
2023-05-12 17:24:08 +0200

    remove polish function

--- init.lua
+++ init.lua
@@ -14,19 +14,5 @@ return {
             shiftwidth = 4,
             tabstop = 4
         }
-    },
-
-    polish = function()
-        vim.api.nvim_create_autocmd("VimEnter", {
-            desc = 'replace alpha footer text',
-            callback = function()
-                vim.api.nvim_create_autocmd("UIEnter", {
-                    callback = function()
-                        local dashboard = require("alpha.themes.dashboard")
-                        dashboard.section.footer.val = {}
-                    end
-                })
-            end
-        })
-    end
+    }
 }
